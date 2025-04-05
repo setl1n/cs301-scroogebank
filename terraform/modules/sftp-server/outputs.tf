20fba707a98982520dbaa4f5c1bd2d991e24efee
saplingonfire <minhan.ho.2023@scis.smu.edu.sg>
2025-04-05 17:09:25 +0800

    fix: isolated output in \sftp-server

--- terraform/modules/sftp-server/outputs.tf
+++ terraform/modules/sftp-server/outputs.tf
@@ -2,8 +2,3 @@ output "sftp_server_public_ip" {
   value       = aws_instance.sftp_server.public_ip
   description = "Public IP of the SFTP server"
 }
-
-output "sftp_sg_id" {
-  value       = aws_security_group.sftp_sg.id
-  description = "Security group ID for the SFTP server"
-}
