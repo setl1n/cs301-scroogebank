533ee6ab1eb4ff75346e565c4470e50e753e7789
Yekai <119912911+yekaiwu@users.noreply.github.com>
2025-04-09 15:38:22 +0800

    Debug Health Check V2

--- backend/client/src/main/java/com/cs301g2t1/client/security/SecurityConfig.java
+++ backend/client/src/main/java/com/cs301g2t1/client/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("/h2-console/**").permitAll();
             auth.requestMatchers("/clients/**").permitAll();
-            auth.requestMatchers("/health").permitAll();
+            auth.requestMatchers("/api/v1/health").permitAll();
             auth.anyRequest().authenticated();
         });
 
