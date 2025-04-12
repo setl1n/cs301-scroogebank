ec74551ff6757d430f2897291bf8bb91c8c01556
setl1n <kaungsetlin2008@gmail.com>
2025-04-12 11:44:56 +0800

    frontend: sleeker colours

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -2,8 +2,8 @@ import AdminDashboard from './components/AdminDashboard'
 
 const AdminPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black p-4 dark">
-      <div className="max-w-4xl w-full h-[800px] bg-zinc-900 p-8 rounded-lg border border-zinc-800">
+    <div className="min-h-screen flex items-center justify-center bg-[#0A0A0A] p-4 dark">
+      <div className="max-w-4xl w-full h-[800px] bg-[#111214] p-8 rounded-lg border border-[#1E2023] shadow-lg">
         <AdminDashboard />
       </div>
     </div>
